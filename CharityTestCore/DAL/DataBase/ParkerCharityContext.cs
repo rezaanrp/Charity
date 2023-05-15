0f09d02ef40da6c83fd52dccb648f3d2b833b9e0
rezaanrp <rezaanrp@yahoo.com>
2023-05-15 12:31:31 +0330

    Remove

--- CharityTestCore/DAL/DataBase/ParkerCharityContext.cs
+++ CharityTestCore/DAL/DataBase/ParkerCharityContext.cs
@@ -24,8 +24,8 @@ namespace DAL.DataBase
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //   base.OnConfiguring(optionsBuilder);
-           optionsBuilder.UseSqlServer(@"Server=127.0.0.1;TrustServerCertificate=True;Initial Catalog=ParkerCharityDB;User ID=sa;Password=K500at10");
-         //  optionsBuilder.UseSqlServer(@"Server=.;TrustServerCertificate=True;Initial Catalog=ParkerCharityDB;User ID=parker;Password=o$$5dE4g984354$$");
+           optionsBuilder.UseSqlServer(@"Server=127.0.0.1;TrustServerCertificate=True;Initial Catalog=ParkerCharityDB;User ID=;Password=");
+         //  optionsBuilder.UseSqlServer(@"Server=.;TrustServerCertificate=True;Initial Catalog=ParkerCharityDB;User ID=;Password=");
 
         }
     }
