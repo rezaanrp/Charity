684106c8b5f6035403b8979f1e4b18002dd07463
rezaanrp <rezaanrp@yahoo.com>
2024-02-03 13:44:36 +0330

    111

--- CharityTestCore/CharityTestCore/obj/Debug/net7.0/CharityTestCore.AssemblyInfo.cs
+++ CharityTestCore/CharityTestCore/obj/Debug/net7.0/CharityTestCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CharityTestCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b873128360f018c8943fe3b44f2d33c266153b3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CharityTestCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CharityTestCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
